41b699ad318b87869d26a26b0c05f0fdf787c732
Franklin Mensah <fiificode@gmail.com>
2023-11-13 10:38:20 +0000

    Added auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import {Ubuntu} from "next/font/google"
-import './globals.css'
+import type { Metadata } from "next";
+import { Ubuntu } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
-const ubuntu = Ubuntu({ subsets: ['latin'],weight:["300","400","500","700"]})
+import { ClerkProvider } from "@clerk/nextjs";
 
+const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
 
 export const metadata: Metadata = {
-  title: 'ECommerce-CMS',
-  description: 'Admin Dashboard for ECommerce CMS',
-}
+  title: "ECommerce-CMS",
+  description: "Admin Dashboard for ECommerce CMS",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-       className={ubuntu.className}>{children}
-      </body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={ubuntu.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
